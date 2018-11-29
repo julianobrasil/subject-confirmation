947ffb29ed1944b8efb82f66d77331173fb1121c
Juliano <juliano.brasil@gmail.com>
2018-11-29 14:03:06 -0200

    path

--- src/app/subject-confirmation/subject-cell/info-popover/pipes/merged-class-friendly-text.pipe.ts
+++ src/app/subject-confirmation/subject-cell/info-popover/pipes/merged-class-friendly-text.pipe.ts
@@ -1,9 +1,7 @@
 import {Pipe, PipeTransform} from '@angular/core';
 
-import {
-  MergedTimeLine,
-  SagaCourseType,
-} from 'src/app/subject-confirmation/subject-confirmation.service';
+import {MergedTimeLine, SagaCourseType} from '../../../subject-confirmation.service';
+
 
 @Pipe({
   name: 'mergedClassFriendlyText',
